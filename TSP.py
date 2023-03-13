45ca44236605d422a5f7a4d18e8dd63e27c8b1e1
drewleean1 <drewleean@gmail.com>
2023-03-13 16:03:50 -0600

    final draft with comments.

--- TSP.py
+++ TSP.py
@@ -4,9 +4,9 @@
 #Due date: March 13, 2023
 
 def nonZeroMin(input, visited):
-    '''helper function for nextValidMin. this function looks for the next valid min in the neighbors of just one node
+    '''helper function for solve_tsp. this function looks for the next valid min in the neighbors of just one node
     (input). It makes sure that the next valid min is not already visited. Returns a tuple (next valid min, weight)
-    to be used by nextValidMin'''
+    to be used by solve_tsp. Similar function used in my assignment 7'''
     minimum_value = (0, float('inf'))                           #tuple (next valid min, weight)
     for x in range(len(input)):                                 #go through all neighbors of input node
         if input[x] != 0 and input[x] < minimum_value[1]:       #check weights
@@ -16,24 +16,21 @@ def nonZeroMin(input, visited):
     return minimum_value
 
 def solve_tsp(G):
+    '''function that implements the Nearest Neighbor heurisitc to solve the TSP for a given graph. Assumes that there is
+    a possible TSP solution for given G'''
     current_vertex = G[0]
     visited = [0]
+
     while len(visited) < len(G):
+        #while loop that looks for the nearest neighbor of each node
         next_vertex = nonZeroMin(current_vertex, visited)
         visited.append(next_vertex[0])
         current_vertex = G[next_vertex[0]]
+
+    #connect the last node to the starting node
     visited = visited[1:]
     next_vertex = nonZeroMin(current_vertex, visited)
     visited.append(next_vertex[0])
     visited.insert(0, 0)
+
     return visited
-
-G = [
- [0, 2, 3, 20, 1],
- [2, 0, 15, 2, 20],
- [3, 15, 0, 20, 13],
- [20, 2, 20, 0, 9],
- [1, 20, 13, 9, 0],
-]
-
-print(solve_tsp(G))
